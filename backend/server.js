c1789dcbafd4ef65048cfebf716b8b5757833144
tranh0anghuan <121786621+tranh0anghuan@users.noreply.github.com>
2024-08-10 19:26:15 +0700

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 
-
 import { ENV_VARS } from "./config/envVar.js";
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //will allow us to parse req.body
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Server is ready 123");
 });
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
   connectDB();
 });
-
-
